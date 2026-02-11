f5ad77613d750313b07c7889787f1aca872383cb
Thuan Vo <thvo@redhat.com>
2026-02-11 15:11:32 -0800

    iam: use ec2.amazonaws.com as service principal
    EUS partition also uses amazonaws.com suffix similar to global
    partition. If using amazonaws.eu, the following error occured.
    
    MalformedPolicyDocument: Invalid principal in policy: "SERVICE":"ec2.amazonaws.eu"

--- pkg/types/aws/regions.go
+++ pkg/types/aws/regions.go
@@ -88,6 +88,7 @@ const (
 	AwsUsGovPartitionID = "aws-us-gov" // AWS GovCloud (US) partition.
 	AwsIsoPartitionID   = "aws-iso"    // AWS ISO (US) partition.
 	AwsIsoBPartitionID  = "aws-iso-b"  // AWS ISOB (US) partition.
+	AwsEuscPartitionID  = "aws-eusc"   // AWS Europe Sovereign Cloud.
 )
 
 // AWS Standard partition's regions.
